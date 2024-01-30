c9ec28dda1806f8d973c3ce89351b89ae2eae5de
Clockwork <alex@clockwork.gr>
2024-01-30 10:52:53 +0200

    test: test linting results (#8)
    * test: test linting results
    
    * Update src/utils/index.ts
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * chore: Change linting action
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 export function toHexString(byteArray: ArrayLike<number>) {
   return Array.prototype.map
     .call(byteArray, function (byte) {
-      return ('0' + (byte & 0xff).toString(16)).slice(-2);
+      return ("0" + (byte & 0xff).toString(16)).slice(-2);
     })
-    .join("");
+       .join("");
 }
